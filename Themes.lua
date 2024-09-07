3a37bba1e186e97e2ed6c55d7417daccd79c3012
DarkMoon X <157520368+Darkmoonxhubscript@users.noreply.github.com>
2024-09-07 18:00:26 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -15,57 +15,6 @@ local Themes = {
     ["Stroke Thickness"] = 1,
     ["Tween Style"] = Enum.EasingStyle.Quad
   },
-
-  ["White"] = {
-    ["Color Hub 1"] = Color3.fromRGB(255, 255, 255),
-    ["Color Hub 2"] = Color3.fromRGB(240, 240, 240),
-    ["Color Theme"] = Color3.fromRGB(0, 150, 255),
-    ["Color Text"] = Color3.fromRGB(0, 0, 0),
-    ["Color Dark Text"] = Color3.fromRGB(50, 50, 50),
-    ["Color Stroke"] = Color3.fromRGB(200, 200, 200),
-    ["Corner Radius"] = UDim.new(0, 5),
-    ["Font"] = {
-      Enum.Font.SourceSans,
-      Enum.Font.SourceSansBold,
-      Enum.Font.SourceSansLight
-    },
-    ["Stroke Thickness"] = 1,
-    ["Tween Style"] = Enum.EasingStyle.Linear
-  },
-
-  ["Grape"] = {
-    ["Color Hub 1"] = Color3.fromRGB(50, 0, 50),
-    ["Color Hub 2"] = Color3.fromRGB(80, 0, 80),
-    ["Color Theme"] = Color3.fromRGB(120, 0, 120),
-    ["Color Text"] = Color3.fromRGB(255, 255, 255),
-    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
-    ["Color Stroke"] = Color3.fromRGB(100, 0, 100),
-    ["Corner Radius"] = UDim.new(0, 5),
-    ["Font"] = {
-      Enum.Font.GothamBlack,
-      Enum.Font.GothamBold,
-      Enum.Font.GothamMedium
-    },
-    ["Stroke Thickness"] = 1,
-    ["Tween Style"] = Enum.EasingStyle.Sine
-  },
-
-  ["Amoled"] = {
-    ["Color Hub 1"] = Color3.fromRGB(0, 0, 0),
-    ["Color Hub 2"] = Color3.fromRGB(10, 10, 10),
-    ["Color Theme"] = Color3.fromRGB(30, 30, 30),
-    ["Color Text"] = Color3.fromRGB(255, 255, 255),
-    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
-    ["Color Stroke"] = Color3.fromRGB(30, 30, 30),
-    ["Corner Radius"] = UDim.new(0, 5),
-    ["Font"] = {
-      Enum.Font.GothamBlack,
-      Enum.Font.GothamBold,
-      Enum.Font.GothamMedium
-    },
-    ["Stroke Thickness"] = 1,
-    ["Tween Style"] = Enum.EasingStyle.Circular
-  },
 }
 
 return Themes
