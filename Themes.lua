dc03499b7e6d4373047d78913f675b6655bf0ffe
DarkMoon X <157520368+Darkmoonxhubscript@users.noreply.github.com>
2024-09-07 17:27:00 -0300

    Update Themes.lua

--- Themes.lua
+++ Themes.lua
@@ -1,5 +1,5 @@
 local Themes = {
-  ["Default"] = {
+  ["Black"] = {
     ["Color Hub 1"] = Color3.fromRGB(18, 18, 18),
     ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
     ["Color Theme"] = Color3.fromRGB(30, 150, 240),
@@ -14,6 +14,57 @@ local Themes = {
     },
     ["Stroke Thickness"] = 1,
     ["Tween Style"] = Enum.EasingStyle.Quad
+  },
+
+  ["White"] = {
+    ["Color Hub 1"] = Color3.fromRGB(255, 255, 255),
+    ["Color Hub 2"] = Color3.fromRGB(240, 240, 240),
+    ["Color Theme"] = Color3.fromRGB(0, 150, 255),
+    ["Color Text"] = Color3.fromRGB(0, 0, 0),
+    ["Color Dark Text"] = Color3.fromRGB(50, 50, 50),
+    ["Color Stroke"] = Color3.fromRGB(200, 200, 200),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.SourceSans,
+      Enum.Font.SourceSansBold,
+      Enum.Font.SourceSansLight
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Linear
+  },
+
+  ["Grape"] = {
+    ["Color Hub 1"] = Color3.fromRGB(50, 0, 50),
+    ["Color Hub 2"] = Color3.fromRGB(80, 0, 80),
+    ["Color Theme"] = Color3.fromRGB(120, 0, 120),
+    ["Color Text"] = Color3.fromRGB(255, 255, 255),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(100, 0, 100),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Sine
+  },
+
+  ["Amoled"] = {
+    ["Color Hub 1"] = Color3.fromRGB(0, 0, 0),
+    ["Color Hub 2"] = Color3.fromRGB(10, 10, 10),
+    ["Color Theme"] = Color3.fromRGB(30, 30, 30),
+    ["Color Text"] = Color3.fromRGB(255, 255, 255),
+    ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
+    ["Color Stroke"] = Color3.fromRGB(30, 30, 30),
+    ["Corner Radius"] = UDim.new(0, 5),
+    ["Font"] = {
+      Enum.Font.GothamBlack,
+      Enum.Font.GothamBold,
+      Enum.Font.GothamMedium
+    },
+    ["Stroke Thickness"] = 1,
+    ["Tween Style"] = Enum.EasingStyle.Circular
   },
 }
 
